3a919466b97637993dbd9f218a7e4a23b73cb152
varnikarawat <50014802+varnikarawat@users.noreply.github.com>
2019-05-03 18:15:35 +0530

    Update create.php

--- create.php
+++ create.php
@@ -1,1 +1,19 @@
-Database
+
+<?php
+$servername='localhost';
+$userrname='root';
+$password='';
+$conn=new mysqli($servername,$userrname,$password);
+if($conn->connect_error){
+    echo("connection failed:" .$conn->connect_error);
+
+}
+$sql="create database demo";
+if($conn->query($sql) == TRUE){
+    echo("database created successfully");
+
+} else {
+    echo "Error creating database: " .$conn ->error;
+}
+$conn->close();
+?>
